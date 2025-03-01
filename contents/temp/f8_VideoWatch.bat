63faf8237b2f8ebc5c8aab9bafedf109e4606bcf
Vu Van Nghia <vuvannghia.work@gmail.com>
2025-03-01 21:13:10 +0700

    build: update f8_VideoWatch.bat and f9_VideoWatch.bat

--- contents/temp/f8_VideoWatch.bat
+++ contents/temp/f8_VideoWatch.bat
@@ -1,7 +1,6 @@
 @REM echo %cd%
-cd C:\Users\Admin\Downloads\NghiaDownloads\NghiaGit\NghiaGitlab\whynotnghiavu-private\course\tools\videowatch\code
-@REM echo %cd%
-python C:\Users\Admin\Downloads\NghiaDownloads\NghiaGit\NghiaGitlab\whynotnghiavu-private\course\tools\videowatch\code\main8.py
+cd C:\Users\Admin\Downloads\videowatch-main\videowatch-main\code
+python C:\Users\Admin\Downloads\videowatch-main\videowatch-main\code\main8.py
 @REM pause
 
 
--- contents/temp/f9_VideoWatch.bat
+++ contents/temp/f9_VideoWatch.bat
@@ -1,6 +1,6 @@
 @REM echo %cd%
-cd C:\Users\Admin\Downloads\NghiaDownloads\NghiaGit\NghiaGitlab\whynotnghiavu-private\course\tools\videowatch\code
-python C:\Users\Admin\Downloads\NghiaDownloads\NghiaGit\NghiaGitlab\whynotnghiavu-private\course\tools\videowatch\code\main9.py
+cd C:\Users\Admin\Downloads\videowatch-main\videowatch-main\code
+python C:\Users\Admin\Downloads\videowatch-main\videowatch-main\code\main9.py
 @REM pause
 
 
