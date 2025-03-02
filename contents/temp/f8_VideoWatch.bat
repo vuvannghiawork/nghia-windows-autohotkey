5fc0e0f6ac6d636842dde6c6617e05b83422b460
Vu Van Nghia <vuvannghia.work@gmail.com>
2025-03-02 18:18:00 +0700

    build: update f8_VideoWatch.bat and f9_VideoWatch.bat

--- contents/temp/f8_VideoWatch.bat
+++ contents/temp/f8_VideoWatch.bat
@@ -1,6 +1,5 @@
 @REM echo %cd%
-cd C:\Users\Admin\Downloads\videowatch-main\videowatch-main\code
-python C:\Users\Admin\Downloads\videowatch-main\videowatch-main\code\main8.py
+cd C:\Users\Admin\Downloads\tool\_\videowatch-main\videowatch-main\code
+python C:\Users\Admin\Downloads\tool\_\videowatch-main\videowatch-main\code\main8.py
 @REM pause
-
 
--- contents/temp/f9_VideoWatch.bat
+++ contents/temp/f9_VideoWatch.bat
@@ -1,6 +1,6 @@
 @REM echo %cd%
-cd C:\Users\Admin\Downloads\videowatch-main\videowatch-main\code
-python C:\Users\Admin\Downloads\videowatch-main\videowatch-main\code\main9.py
+cd C:\Users\Admin\Downloads\tool\_\videowatch-main\videowatch-main\code
+python C:\Users\Admin\Downloads\tool\_\videowatch-main\videowatch-main\code\main9.py
 @REM pause
 
 
